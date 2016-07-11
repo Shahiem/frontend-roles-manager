8b2e474b4b41dbe501643494a1c75a83264d42d7
Neri Junior <neri@nerijunior.com>
2016-07-11 11:50:57 -0300

    Backend menu

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,9 @@
 <?php
 namespace ShahiemSeymor\Roles;
 
-use App;
-use Event;
 use Backend;
-use BackendMenu;
-use Illuminate\Foundation\AliasLoader;
+use Event;
 use RainLab\User\Models\User;
-use RainLab\User\Components\Account;
-use ShahiemSeymor\Roles\Models\Custom;
 use ShahiemSeymor\Roles\Models\Group;
 use System\Classes\PluginBase;
 
@@ -23,53 +18,55 @@ class Plugin extends PluginBase
             'name'        => 'Frontend User Roles Manager',
             'description' => 'The plugin lets you manage frontend user roles and permissions.',
             'author'      => 'Shahiem Seymor',
-            'homepage'    => 'http://octobercms.com/plugin/shahiemseymor-roles'
+            'homepage'    => 'http://octobercms.com/plugin/shahiemseymor-roles',
         ];
     }
 
     public function boot()
     {
-        User::extend(function($model) 
-        {
+        User::extend(function ($model) {
             $model->belongsToMany['groups']      = ['ShahiemSeymor\Roles\Models\Group', 'table' => 'shahiemseymor_assigned_roles', 'otherKey' => 'role_id'];
-            $model->belongsToMany['permissions'] = ['ShahiemSeymor\Roles\Models\Group', 'table' => 'shahiemseymor_assigned_roles', 'otherKey' => 'role_id'];     
-        });  
+            $model->belongsToMany['permissions'] = ['ShahiemSeymor\Roles\Models\Group', 'table' => 'shahiemseymor_assigned_roles', 'otherKey' => 'role_id'];
+        });
 
         $userGroup = new Group;
         $userGroup->newUserAddToDefaultGroup();
 
-        Event::listen('backend.menu.extendItems', function($manager)
-        {
-           $manager->addSideMenuItems('RainLab.User', 'user', [
+        Event::listen('backend.menu.extendItems', function ($manager) {
+            $manager->addSideMenuItems('RainLab.User', 'user', [
                 'roles' => [
-                    'label'       => 'Roles',
-                    'icon'        => 'icon-users',
-                    'code'        => 'roles',
-                    'owner'       => 'RainLab.User',
-                    'url'         => Backend::url('shahiemseymor/roles/groups')
+                    'label' => trans('shahiemseymor.roles::lang.roles'),
+                    'icon'  => 'icon-users',
+                    'code'  => 'roles',
+                    'owner' => 'RainLab.User',
+                    'url'   => Backend::url('shahiemseymor/roles/groups'),
                 ],
                 'perms' => [
-                    'label'       => 'Permissions',
-                    'icon'        => 'icon-key',
-                    'code'        => 'perms',
-                    'owner'       => 'RainLab.User',
-                    'url'         => Backend::url('shahiemseymor/roles/permissions')
-                ]
+                    'label' => trans('shahiemseymor.roles::lang.permissions'),
+                    'icon'  => 'icon-key',
+                    'code'  => 'perms',
+                    'owner' => 'RainLab.User',
+                    'url'   => Backend::url('shahiemseymor/roles/permissions'),
+                ],
             ]);
         });
 
-        Event::listen('backend.form.extendFields', function($widget) 
-        {
-            if (!$widget->getController() instanceof \RainLab\User\Controllers\Users) return;
-            if (!$widget->model instanceof \RainLab\User\Models\User) return;
-            
-             $widget->addFields([
-                'groups'              => [
-                    'label'           => 'Groups',
-                    'commentAbove'    => 'Specify which groups this person belongs to.',
-                    'tab'             => 'Roles',
-                    'type'            => 'relation'
-                ]
+        Event::listen('backend.form.extendFields', function ($widget) {
+            if (!$widget->getController() instanceof \RainLab\User\Controllers\Users) {
+                return;
+            }
+
+            if (!$widget->model instanceof \RainLab\User\Models\User) {
+                return;
+            }
+
+            $widget->addFields([
+                'groups' => [
+                    'label'        => trans('shahiemseymor.roles::lang.field.groups'),
+                    'commentAbove' => trans('shahiemseymor.roles::lang.field.helper.groups'),
+                    'tab'          => trans('shahiemseymor.roles::lang.roles'),
+                    'type'         => 'relation',
+                ],
             ], 'primary');
         });
     }
@@ -77,10 +74,10 @@ class Plugin extends PluginBase
     public function registerMarkupTags()
     {
         return [
-            'functions'   => [
-                'can'         => function($can) { return Group::can($can); },
-                'hasRole'     => function($role, $user = null) { return Group::hasRole($role, $user); }
-            ]
+            'functions' => [
+                'can'     => function ($can) {return Group::can($can);},
+                'hasRole' => function ($role, $user = null) {return Group::hasRole($role, $user);},
+            ],
         ];
     }
 
